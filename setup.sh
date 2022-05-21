58e0098d29b050935a48774ba80f804398cad3a2
busbormeo <60380171+busbormeo@users.noreply.github.com>
2022-05-21 22:32:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 # Install rclone static binary
 wget -q https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip -q rclone-current-linux-amd64.zip
-export PATH=$PWD/rclone-current-linux-amd64:$PATH
+export PATH=$PWD/rclone-*-linux-amd64:$PATH
